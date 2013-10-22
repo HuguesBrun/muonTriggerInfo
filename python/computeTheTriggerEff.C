cf4c5acced184f86fbd35052c628358e3852bfbd
Hugues <hbrun@cern.ch>
2013-10-22 17:23:55 +0200

    remove the generator cut

--- python/computeTheTriggerEff.C
+++ python/computeTheTriggerEff.C
@@ -67,7 +67,6 @@ computeTheTriggerEff(){
         if (i%1000==0) cout << "event number " << i << endl;
   
         chain->GetEntry(i);
-        if (T_Muon_Pt->size()<2) continue; //need at least 2 muons in the event...
    
     
         
@@ -84,10 +83,11 @@ computeTheTriggerEff(){
             }
         }
        // cout << "nb nbZ genmuons=" << nbPairMuonsFromZ << endl;
-        if (!(nbPairMuonsFromZ>=1)) continue; // if not Z->mu mu gen event then go to next event
+        //if (!(nbPairMuonsFromZ>=1)) continue; // if not Z->mu mu gen event then go to next event
         nbEventPassingGenCut++;
         
         
+        if (T_Muon_Pt->size()<2) continue; //need at least 2 muons in the event...
         int nbMuons = T_Muon_Pt->size();
         int nbLooseMuons = 0;
          std::vector<int> refLooseMuons = 0;
